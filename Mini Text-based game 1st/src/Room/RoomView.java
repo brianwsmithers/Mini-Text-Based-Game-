5b28066d925568d3dd56b18b13aca2dca52d83f9
Brian Smithers <56320061+brianwsmithers@users.noreply.github.com>
2022-02-26 16:24:52 -0500

    Delete Mini Text-based game 1st/src/Room directory

--- Mini Text-based game 1st/src/Room/RoomView.java
+++
@@ -1,9 +0,0 @@
-package Room;
-
-public class RoomView {
-
-    public void printRoomDetails(RoomModel roomModel) {
-        System.out.println(roomModel);
-        System.out.println(roomModel.iterateRoomConnection());
-    }
-}
